c2944e2cb071a28a85f45838a3d51d019b5a712d
Beneus <benjaminpenadelahera@gmail.com>
2017-10-15 00:02:43 +0100

    Update HrefLang.php

--- Block/HrefLang.php
+++ Block/HrefLang.php
@@ -51,49 +51,23 @@ class HrefLang extends \Magento\Framework\View\Element\Template
 		$category = $this->_register->registry('current_category');
 		$product = $this->_register->registry('current_product');
 		$stores = $this->_storeManager->getStores();
-		
 		foreach ($stores as $store) {
-				$storeId = $store->getId();
+			$storeId = $store->getId();
+			if ($product) {
+				$productId = $product->getId();
+				$url =  $this->_productFactory->create()->setStoreId($storeId)->load($productId)->getProductUrl();
+			} else if($category){
+				$categoryId = $category->getId();
+				$cat =  $this->_categoryFactory->create()->setStoreId($storeId)->load($categoryId);
+				$url = $cat->getUrl();
+			}else {
+			    $url = $store->getCurrentUrl();
+			}
+			$locale_code = $this->_scopeConfig->getValue('general/locale/code', \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $storeId);
+			$ret .= '<link ref="alternate" hreflang="'. $locale_code .'" href="' . $url . '" />' .PHP_EOL;
+            	}
 
-                if ($product) {
-                	$productId = $product->getId();
-                	$url =  $this->_productFactory->create()->setStoreId($storeId)->load($productId)->getProductUrl();
-                  
-                } else if($category){
-                	 //$category ? $categoryId = $category->getId() : $categoryId = null;
-                	 //$url = $category->setStoreId($store->getId())->getUrlModel()->getUrl();
-                	$categoryId = $category->getId();
-                	$cat =  $this->_categoryRepository->get($categoryId , $storeId);
-                	$cat =  $this->_categoryFactory->create()->setStoreId($storeId)->load($categoryId);
-
-
-
-//$url = $store->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB) . $store->getCode() .'/' . $cat->getUrlKey();
-//$url = $this->_categoryFactory->create()->setData(
-//$this->_categoryRepository->get($categoryId, $storeId)->getData())->getUrl();
-                	$url = $cat->getCategoryIdUrl();
-                	//$url =  $this->_categoryFactory->create()->setStoreId($storeId)->load($categoryId)->getUrl();
-
-                }else {
-                    $url = $store->getCurrentUrl();
-                }
-
-
-               //$storeCode = substr($this->_scopeConfig->getValue('general/locale/code', $store->getId()), 0, 2);
-
-//$objectManager = \Magento\Framework\App\ObjectManager::getInstance(); 
-//$store = $objectManager->get('Magento\Framework\Locale\Resolver'); 
-
-
-$locale_code = $this->_scopeConfig->getValue('general/locale/code', \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $storeId);
-
-$ret .= '<link ref="alternate" hreflang="'. $locale_code .'" href="' . $url . '" />' .PHP_EOL;
-            }
-
-           $ret.= PHP_EOL; 
-         return $ret;
-
+        	$ret.= PHP_EOL; 
+         	return $ret;
 	}
-
-	
 }
